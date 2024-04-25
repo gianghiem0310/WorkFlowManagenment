b8cacf5577ffb2492fb69273118d1f03e7807e19
phucnh0707 <nguyenhphuc0707@gmail.com>
2024-04-25 07:36:46 -0700

    Giao dien tao cong

---
+++ GroupManager/GroupManager/TaoCongViecViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaoCongViecViewController.swift
+//  GroupManager
+//
+//  Created by Nguyen Hong Phuc on 4/25/24.
+//
+
+import UIKit
+
+class TaoCongViecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
