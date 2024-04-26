731e9dd3a05116efcff05460a28a6d7a156b285c
gianghiem0310 <jangiim93nt@gmail.com>
2024-04-26 06:03:49 -0700

    Merge branch 'Group/NguyenGiaNghiem' into Test

---
+++ GroupManager/GroupManager/CongViecDangLamCell.swift
@@ -0,0 +1,23 @@
+//
+//  CongViecDangLamCell.swift
+//  GroupManager
+//
+//  Created by Gia Nghiem on 4/24/24.
+//
+
+import UIKit
+
+class CongViecDangLamCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
